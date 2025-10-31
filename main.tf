4da046375b494f7813fe9297ad7239fec793ae84
stroievlad99 <stroie.vlad99@gmail.com>
2025-10-31 14:17:19 +0100

    adding env vars for terraform actions

--- main.tf
+++ main.tf
@@ -138,12 +138,12 @@ resource "random_id" "suffix" {
 }
 
 # Creează bucket-ul S3 fără acl (argumentul acl e deprecated)
-resource "aws_s3_bucket" "dummy_bucket_1" {
+resource "aws_s3_bucket" "dummy_bucket" {
   bucket = "terraform-test-bucket-${random_id.suffix.hex}"
 }
 
 # Setează ACL separat
 resource "aws_s3_bucket_acl" "dummy_bucket_acl" {
-  bucket = aws_s3_bucket.dummy_bucket_1.id
+  bucket = aws_s3_bucket.dummy_bucket.id
   acl    = "private"
 }
